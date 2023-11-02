9154754be21df79d32effd4670b55cdd0ead9120
chibancas <juanjosefdz2003@gmail.com>
2023-11-02 13:08:15 +0100

    no funciona bien configmodule en app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type:'postgres',
-      
+      host:process.env.DB_HOST,
+      port:+process.env.DB_PORT,
+      database:process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities:true,
+      synchronize:true
+
+
+
     })
   ],
   controllers: [AppController],
